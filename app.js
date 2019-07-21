c417c398ad37cf417b06b988244a249c6a9aeebe
Bartłomiej Kotarski <bkotarski1@gmail.com>
2019-07-21 19:57:54 +0200

    Code clean

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const token = require("./configKey");
+const discord = require('./discord');
+const token = require('./configKey');
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
-  
-  client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
-  });
-  
-
-    console.log(token);
-    client.login(token);
-  
+discord.bot.login(token)
